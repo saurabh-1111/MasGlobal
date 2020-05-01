30614d71605eafecd582185d834d52a7d4f33025
saurabh-1111 <62569819+saurabh-1111@users.noreply.github.com>
2020-05-01 00:13:26 -0500

    SpringBoot starting class created

---
+++ src/main/java/masglobaltestapi/azurewebsites/net/EmployeeRun.java
@@ -0,0 +1,15 @@
+package masglobaltestapi.azurewebsites.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRun {
+    
+    public static void main(String[] args) {
+
+        SpringApplication.run(EmployeeRun.class, args);
+        
+    }
+    
+}
